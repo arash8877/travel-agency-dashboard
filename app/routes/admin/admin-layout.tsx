6fd45a0fe81f913ec95b6ffedbe0e31096cda828
Arash <arash8877@gmail.com>
2025-05-19 11:46:40 +0200

    routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,14 @@
+
+import {Outlet} from "react-router";
+
+const AdminLayout = () => {
+    return (<div className="admin-layout">
+        Mobile Sidebar
+        <aside className="w-full max-w-[720px] hidden lg:block">Sidebar</aside>
+        <aside className="children">
+            <Outlet />
+        </aside>
+    </div>);
+}
+
+export default AdminLayout;
